a267d35a69b53ef376b8f078bd168c3f5aeb1b73
KidmanXD <112696687+KidmanXD@users.noreply.github.com>
2023-01-24 14:22:49 +0200

    Add files via upload

---
+++ Pangram/tests/Pangramtest.py
@@ -0,0 +1,18 @@
+import pytest
+from Pangram.src.Pangramcode import *
+
+
+@pytest.mark.test1
+def test_pangram_success():
+    res = pangram("ff")
+    assert res == False
+
+
+@pytest.mark.test2
+def test_pangram_failure():
+    res = pangram("The quick brown fox jumps over the lazy dog.")
+    assert res == True
+
+
+if __name__ == '__main__':
+    pytest.main()
