4eb3bc5b4a32ee6403a67caaf10b56894349da9b
KidmanXD <112696687+KidmanXD@users.noreply.github.com>
2023-01-24 14:24:54 +0200

    Update Pangramtest.py

--- Pangram/tests/Pangramtest.py
+++ Pangram/tests/Pangramtest.py
@@ -3,13 +3,13 @@ from Pangram.src.Pangramcode import *
 
 
 @pytest.mark.test1
-def test_pangram_success():
+def test_pangram_failure():
     res = pangram("ff")
     assert res == False
 
 
 @pytest.mark.test2
-def test_pangram_failure():
+def test_pangram_success():
     res = pangram("The quick brown fox jumps over the lazy dog.")
     assert res == True
 
